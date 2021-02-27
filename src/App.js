53a968f47cbac0b2228ce5c4862f8d861de56549
yumauma20 <71yuma10@gmail.com>
2021-02-27 18:19:35 +0900

    リポジトリ初期化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import TodoList from "./TodoList"
-
 function App() {
   return (
     <div className="App">
-      <TodoList />
     </div>
   );
 }
